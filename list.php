a73c3f7f76a2f1b8bbc39fc551138bb9234de234
badr1600 <ahmed.amh.badr123@gmail.com>
2019-01-29 00:39:51 -0500

    add download

---
+++ list.php
@@ -0,0 +1,19 @@
+<?php
+require('vendor/autoload.php');
+// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+$s3 = Aws\S3\S3Client::factory();
+$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+?>
+<html>
+    <head><meta charset="UTF-8"></head>
+    <body>
+        <h1>S3 Download example</h1>
+<?php
+
+		$objects = $s3->listObjectsV2($bucket);
+		foreach ($objects['Contents'] as $object){
+			echo "{$object['Key']}\t{$object['LastModified']}\n";
+		}
+?>
+    </body>
+</html>
